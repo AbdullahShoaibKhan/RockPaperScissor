a20e6f878d73e301b3838e951b769739476866a8
AbdullahShoaib <abdullahshoaib0333@gmail.com>
2021-08-22 17:52:11 +0500

    y

---
+++ script.js
@@ -0,0 +1,40 @@
+let playerscore=0;
+let computerscore=0;
+
+function start(){
+    console.log("hello");
+    scoreboard();
+    if (playerscore && computerscore !== 5 ? play(): gameover());
+}
+function play(){
+playerselection();
+computerselection();
+
+}
+
+function playerselection(){
+
+}
+function computerselection(){
+    
+    return computerselection;
+};
+function checkselection(playerselection, computerselection){
+if (playerselection===computerselection){
+     
+}else
+    if (
+    playerselection=== "rock"&& computerselection==="scissor",
+    playerselection=== "scissor"&& computerselection==="paper",
+    playerselection=== "paper"&& computerselection==="rock"
+){playerscore++;
+return playerscore;}
+else if() {
+computerscore++;
+return computerscore;
+}}
+function scoreboard(playerscore,computerscore){
+    
+}
+result
+
