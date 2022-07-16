22893c1660d5fcc62c7b5a8bd857814c98ca48be
AbdullahShoaib <abdullahshoaib0333@gmail.com>
2022-07-16 21:29:04 +0500

    final commit

--- script.js
+++ script.js
@@ -10,57 +10,73 @@ buttons=document.querySelectorAll('.button');
 let rock=document.querySelector('#rock');
 let paper=document.querySelector('#paper');
 let scissor=document.querySelector('#scissor');
+let comrock=document.querySelector('#c-rock');
+let compaper=document.querySelector('#c-paper');
+let comscissor=document.querySelector('#c-scissor');
 let start=document.querySelector('#start');
 start.addEventListener('click',playerselection);
+
+// function player(){
+//     console.log(playerSelection);
+//     console.log('playerSelection');
+// }
 
 function playerselection(){
 start.textContent="FIGHT";
 rock.addEventListener('click',()=>{
     playerSelection='rock';
     computerselection();
-        console.log(playerSelection);
-        console.log('playerSelection');
+   // player(playerSelection);
         comparison(playerSelection,computerSelection);
 })
 paper.addEventListener('click',()=>{
     playerSelection='paper';
     computerselection();
-        console.log(playerSelection);
-        console.log('playerSelection');
+   // player(playerSelection);
         comparison(playerSelection,computerSelection);
 })
 scissor.addEventListener('click',()=>{
     playerSelection='scissor';
     computerselection();
-        console.log(playerSelection);
-        console.log('playerSelection');
+   // player(playerSelection);
         comparison(playerSelection,computerSelection);
-        winner(playerscore,computerscore);
 })
-
 }
-
 
 function computerselection(){
 options=['rock','paper','scissor'];
 computerSelection=options[~~(Math.random()*options.length)];
-    console.log(computerSelection);
-    console.log('Computer Selection');
+
+if (computerSelection=="rock"){
+    comrock.style.border = "thick solid white";
+        compaper.style.border = "2px solid black";
+        comscissor.style.border = "2px solid black";
+}
+else if (computerSelection=="paper"){
+    compaper.style.border = "thick solid white";
+        comrock.style.border = "2px solid black";
+        comscissor.style.border = "2px solid black";
+}
+else if (computerSelection=="scissor"){
+    comscissor.style.border = "thick solid white";
+        comrock.style.border = "2px solid black";
+        compaper.style.border = "2px solid black";
+}
+   // console.log(computerSelection);
+   // console.log('Computer Selection');
        return computerSelection;
 }
 
-
-
 function comparison(playerSelection,computerSelection){
 if(computerSelection===playerSelection){
-    console.log('tie');
+   // console.log('tie');
     document.getElementById('comment').innerText="It's a tie" ;
+   
 } else if (
     playerSelection == "rock" && computerSelection=="scissor"||
     playerSelection == "scissor" && computerSelection=="paper"||
     playerSelection == "paper" && computerSelection=="rock" 
     ){
-    console.log('WoN');
     playerscore++;
     document.getElementById('comment').innerText="1 Point for Player" ;
     document.getElementById('playerscr').innerText="Player Score :"+playerscore ;
@@ -71,11 +87,10 @@ if(computerSelection===playerSelection){
     playerSelection=== "paper"&& computerSelection==="scissor"||
     playerSelection=== "rock"&& computerSelection==="paper"
     ){
-    console.log('lost');
     computerscore++;
     document.getElementById('comment').innerText="1 Point for Computer" ;
     document.getElementById('computerscr').innerText="Computer Score :"+computerscore;
-    console.log(computerscore);
+   // console.log(computerscore);
     winner(playerscore,computerscore);
         return computerscore;
 }
@@ -84,8 +99,8 @@ if(computerSelection===playerSelection){
 function clear(){
     playerscore = 0;
     computerscore = 0;
-    console.log(playerscore);
-    console.log(computerscore);
+   // console.log(playerscore);
+   // console.log(computerscore);
 
     document.getElementById('comment').innerText="";
     document.getElementById('computerscr').innerText="Computer Score :"+computerscore;
@@ -96,19 +111,18 @@ function on(){
     document.getElementById("overlayCmt").innerText=champ;
 }
 function off(){
-    console.log('yo');
     document.getElementById("overlay").style.display="none";
     clear(computerscore,playerscore);
 }
 function winner(playerscore,computerscore){
 if (playerscore === 5){
-    console.log('player won');
+   // console.log('player won');
     document.getElementById('comment').innerText="Player got to 5 points so Player Wins";
     champ= "Player Won";
     on(champ);
 }
 else if (computerscore === 5){
-    console.log('computer won');
+   // console.log('computer won');
     document.getElementById('comment').innerText="Computer got to 5 points so Computer Wins";
     champ= "Computer Won";
     on(champ);
